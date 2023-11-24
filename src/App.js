36a92a90b7c036434e285fb4aaa68ddae79b9329
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-24 05:15:56 -0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const base_url = 'https://railwaydrf-production.up.railway.app/index/';
+
+  useEffect(() => {
+    axios.get(base_url)
+      .then(response => {
+        console.log('Data from API:', response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Soy Franco
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Estos son los resultados:</h1>
+      
     </div>
   );
 }
