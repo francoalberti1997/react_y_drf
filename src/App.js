bbde7310e64c2011248b751fce03a70345bae58f
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-24 21:21:03 -0300

    actualizando la page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const base_url = 'https://railwaydrf-production.up.railway.app/index/';
+  const base_url = 'https://railwaydrf-production.up.railway.app/api/propuestas/';
 
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     axios.get(base_url)
@@ -20,7 +20,16 @@ function App() {
   return (
     <div> 
       <h1>Estos son los resultados:</h1>
-      <h2>{data.message}</h2>
+      {data.map((item) => (
+        <div key={item.id}>
+          <p>ID: {item.id}</p>
+          <p>Nombre: {item.nombre}</p>
+          <p>Apellido: {item.apellido}</p>
+          <p>Edad: {item.edad}</p>
+          <p>Propuesta: {item.propuesta}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
